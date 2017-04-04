49421a581478a946301b7010ac74048b5b42a812
Denis Lunev <den.lunev@gmail.com>
2017-04-04 12:01:38 -0400

    Site update: 2017-04-04 12:01:35

---
+++ js/payment.js
@@ -0,0 +1,8 @@
+DepositFixForm.init({
+    hubSpotFormId: "63ed979f-7148-40ae-84f7-fa720e96894f",
+    userId: '7889f7d8-d8e3-4448-808b-1ad756761ca0',
+    amount: '50',
+    productName: 'Monthly Subscription',
+    discount: false,
+    currency: 'USD'
+});
